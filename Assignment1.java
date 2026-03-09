435d2e056eee4948b4b0c3d5b12bdd42ea87e11f
lanch-005 <lanchana005@gmail.com>
2026-03-09 15:33:31 +0000

    Assignment1 on Arrays Submission

--- Assignment1.java
+++ Assignment1.java
@@ -1,8 +1,19 @@
 class Assignment1 {
   public static int[] findLeapYears(int year) {
-  // Implement your code here and change the return value accordingly
+
     
-  return null;
+    int[] arr = new int[15];
+    int i = 0;
+
+    while(i < 15){
+        if(year % 400 == 0 || (year % 4 == 0 && year % 100 != 0)){
+            arr[i] = year;
+            i++;
+        }
+        year++;
+    }
+
+    return arr;
   }
   
   public static void main(String[] args) {
